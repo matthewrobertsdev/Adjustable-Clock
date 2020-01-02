7eb9967bf80cec1d0854837fc6102bd0134bdb3d
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-01-02 17:41:29 -0500

    hacky

--- Adjustable Clock/Digital Clock/AnalogClockView.swift
+++ Adjustable Clock/Digital Clock/AnalogClockView.swift
@@ -7,9 +7,18 @@
 //
 import Cocoa
 class AnalogClockView: NSView {
+	@IBOutlet weak var twelfthLabel: NSTextField!
+	@IBOutlet weak var myConstraint : NSLayoutConstraint!
+	@IBOutlet weak var funView: NSView!
+	@IBOutlet weak var myConstraint1 : NSLayoutConstraint!
+	@IBOutlet weak var myConstraint2 : NSLayoutConstraint!
+	
 	var color=NSColor.labelColor
+	var done=false
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
+		funView.wantsLayer=true
+		funView.layer?.backgroundColor=NSColor.blue.cgColor
 		let origin=CGPoint(x: frame.width*0.05, y: frame.height*0.05)
 		let path=NSBezierPath(ovalIn: NSRect(origin: origin, size: CGSize(width: frame.width*0.9, height: frame.height*0.9)))
 		color.setStroke()
@@ -28,6 +37,21 @@ class AnalogClockView: NSView {
 		for minute in 0...60 {
 			drawDash(cgContext: cgContext, angle: CGFloat(2*Double.pi*Double(minute)/60), startProportion: 0.6)
 		}
+		self.myConstraint.constant = frame.width/2
+		//if(!done){
+			//self.twelfthLabel.scaleUnitSquare(to: NSSize(width: frame.width/(twelfthLabel.bounds.width*5), height: frame.width/(twelfthLabel.bounds.width*5)))
+		//twelfthLabel.setBoundsSize(NSSize(width: 2, height: 2))
+			done=true
+		//}
+		let rect=NSRect(origin: CGPoint(x: frame.width/2, y:self.twelfthLabel.bounds.width/2), size: CGSize(width: frame.width/2, height: frame.height/2))
+		self.twelfthLabel.bounds=rect
+		twelfthLabel.scaleUnitSquare(to: NSSize(width: frame.width/100, height: frame.width/100))
+		twelfthLabel.setNeedsDisplay(rect)
+		self.myConstraint1.constant = frame.width
+		self.myConstraint2.constant = frame.height
+		//twelfthLabel.layout()
+		//self.funView.layout()
+		//funView.heightAnchor.constraint(equalToConstant: frame.height/2)
     }
 	func drawDash(cgContext: CGContext, angle: CGFloat, startProportion: CGFloat) {
 		let line = CGMutablePath()
