35ad8b18358bcef2ff1b5872fa34915a0dd66c80
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-03-13 13:25:03 -0400

    Fixed bug in timer

--- Adjustable Clock/Timers/TimersViewController.swift
+++ Adjustable Clock/Timers/TimersViewController.swift
@@ -149,7 +149,7 @@ class TimersViewController: ColorfulViewController, NSCollectionViewDataSource, 
 		}
 		if TimersCenter.sharedInstance.timers[index].active {
 			TimersCenter.sharedInstance.timers[index].active=false
-			TimersCenter.sharedInstance.gcdTimers[index].cancel()
+			TimersCenter.sharedInstance.gcdTimers[index].suspend()
 			timerCollectionViewItem.startPauseButton.title="Resume"
 		} else {
 		TimersCenter.sharedInstance.timers[index].active=true
