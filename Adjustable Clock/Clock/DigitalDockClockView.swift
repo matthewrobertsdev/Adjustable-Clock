3267b64ffa3d722a226daba25aeb036686245ac3
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-04-14 13:18:09 -0400

    Fixed dock clock colors and color menu wording.

--- Adjustable Clock/Clock/DigitalDockClockView.swift
+++ Adjustable Clock/Clock/DigitalDockClockView.swift
@@ -110,10 +110,15 @@ class DigitalDockClockView: NSView {
 			backgroundColor.setFill()
 		}
 		if hasDarkAppearance(view: self) {
-			digitalClock.textColor=NSColor.white
+			if ClockPreferencesStorage.sharedInstance.colorChoice==ColorChoice.white {
+				backgroundColor=NSColor.systemGray
+			}
 		} else {
-			digitalClock.textColor=NSColor.black
+			if ClockPreferencesStorage.sharedInstance.colorChoice==ColorChoice.black {
+				backgroundColor=NSColor.systemGray
+			}
 		}
+		backgroundColor.setFill()
 		path.fill()
 	}
 }
