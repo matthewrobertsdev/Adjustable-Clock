af3181f4754c9aca8651e66093d3964ec9793215
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-04-25 16:34:22 -0400

    Made second hand and minute hand longer and made second hand thinner

--- Adjustable Clock/Clock/AnalogClockView.swift
+++ Adjustable Clock/Clock/AnalogClockView.swift
@@ -47,9 +47,9 @@ class AnalogClockView: BaseAnalogClockView {
 	func startHands(withSeconds: Bool) {
 		clearHands()
 		addHand(handLayer: hourLayer, lengthProportion: 0.75, widthProportion: 1.2)
-		addHand(handLayer: minuteLayer, lengthProportion: 0.8, widthProportion: 1.2)
+		addHand(handLayer: minuteLayer, lengthProportion: 0.9, widthProportion: 1.2)
 		if withSeconds {
-			addHand(handLayer: secondLayer, lengthProportion: 0.7, widthProportion: 0.8)
+			addHand(handLayer: secondLayer, lengthProportion: 0.9, widthProportion: 0.6)
 		}
 	}
 	func clearHands() {
